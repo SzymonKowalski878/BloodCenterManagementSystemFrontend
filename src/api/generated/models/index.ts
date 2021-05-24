2d248c7b328e864fb04e05588fd5bc3a5372c959
Marcin Chełstowski <150426@student.uwm.edu.pl>
2021-05-24 10:40:34 +0200

    ok

--- src/api/generated/models/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './ModelError';
-export * from './Pet';
--- src/api/index.ts
+++
@@ -1,9 +0,0 @@
-// a helper file for the api
-
-import { Configuration, PetsApi } from "./generated";
-
-const config = new Configuration({
-  // you can set base path, api key, auth, etc... here
-});
-
-export const petsApi = new PetsApi(config);
